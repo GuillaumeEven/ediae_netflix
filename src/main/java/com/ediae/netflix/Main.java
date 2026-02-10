d2fe3e6d799e682b4c1d03d5d5e93773ebf869d3
GuillaumeEven <guillaume@evendev.net>
2026-02-10 17:51:02 +0100

    clean(main): quit author name

--- src/main/java/com/ediae/netflix/Main.java
+++ src/main/java/com/ediae/netflix/Main.java
@@ -4,12 +4,6 @@
 
 package com.ediae.netflix;
 
-
-// TODO quit this and change class name to Main
-/**
- *
- * @author sensei
- */
 public class Main {
 
     public static void main(String[] args) {
